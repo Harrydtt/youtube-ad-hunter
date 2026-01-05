c70511a19b265f489cbd069b96dd32c8b4f73e62
harry <harry@Mac-mini.local>
2026-01-05 22:13:56 +0700

    v30: Sync Log Versions - Updated all versions to 30.0 (Link Hunter)

--- content.js
+++ content.js
@@ -353,5 +353,5 @@
         }
     }, 500);
 
-    console.log(`%c[Hunter] v29: Project Phantom Active 👻⚡`, "color: #00ff00; font-weight: bold; font-size: 14px;");
+    console.log(`%c[Hunter] v30: Project Phantom Active 👻⚡`, "color: #00ff00; font-weight: bold; font-size: 14px;");
 })();
